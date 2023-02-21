2e49aa62293c348471432e3275b9c36e09eb1db9
Youngju Jang <joj1043@nate.com>
2023-02-21 13:16:32 +0900

    CH2 진행중

--- src/main/java/com/fastcampus/ch2/Hello.java
+++ src/main/java/com/fastcampus/ch2/Hello.java
@@ -5,10 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Hello {
+	int iv = 10;
+	static int cv = 20;
 	
 	@RequestMapping("/hello")
-	public void main() {
-		System.out.println("hello");
+	private void main() { // 인스턴스 메서드
+		System.out.println("hello-static");
+		System.out.println(cv);
+//		System.out.println(iv); // 가능
 	}
-
+	
+	public static void main2(){ // static 메서드 - cv만 가능
+		System.out.println(cv);
+//		System.out.println(iv); // 에러
+	}
 }
