916b0ca5de5344f42ccb064950702c94765c311b
Youngju Jang <joj1043@nate.com>
2023-03-01 18:51:29 +0900

    17. @RequestParam과 @ModelAttribute

--- src/main/java/com/fastcampus/ch2/YoilTellerMVC4.java
+++ src/main/java/com/fastcampus/ch2/YoilTellerMVC4.java
@@ -7,39 +7,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Calendar;
 
 // 년월일
 @Controller
-public class YoilTellerMVC {
+public class YoilTellerMVC4 {
 	@ExceptionHandler(Exception.class)
      public String catcher(Exception ex) {
     	 ex.printStackTrace();
     	 return "yoilError";
      }
-     @RequestMapping ("/getYoilMVC") // http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1
-     public String main(@RequestParam(required=true) int year, 
-    		 @RequestParam(required=true) int month, 
-    		 @RequestParam(required=true) int day, Model model) throws IOException {
+     @RequestMapping ("/getYoilMVC4") // http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1
+     public String main(Mydate date, Model model) throws IOException {
           // 1. 유효성 검사
-          if(!isValid(year, month, day))
+          if(!isValid(date))
                return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
           // 2. 처리
-          char yoil = getYoil(year, month, day);
+          char yoil = getYoil(date);
      
           // 3. Model에 작업 결과 저장
-          model.addAttribute("year", year);
-          model.addAttribute("month", month);
-          model.addAttribute("day", day);
+          model.addAttribute("myDate", date);
           model.addAttribute("yoil", yoil);
           return "yoil"; // /WEB-INF/views/yoil.jsp
      }
      
-     @RequestMapping ("/getYoilMVCModel") // http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1
+     @RequestMapping ("/getYoilMVC4MAV") // http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1
      public ModelAndView main(int year, int month, int day) throws IOException {
           // 이건 잘 안씀
           ModelAndView mv = new ModelAndView();
@@ -60,15 +53,19 @@ public class YoilTellerMVC {
           
           return mv; // /WEB-INF/views/yoil.jsp
      }
-     
-     private static char getYoil(int year, int month, int day) {
+     private char getYoil(Mydate date){
+          return getYoil(date.getYear(), date.getMonth(), date.getDay());
+     }
+     private char getYoil(int year, int month, int day) {
           Calendar cal = Calendar.getInstance();
           cal.set(year, month - 1, day);
           
           int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
           return " 일월화수목금토".charAt(dayOfWeek);   // 일요일:1, 월요일:2, ...
      }
-     
+     private boolean isValid(Mydate date){
+          return isValid(date.getYear(), date.getMonth(), date.getDay());
+     }
      private boolean isValid(int year, int month, int day) {
           if(year==-1 || month==-1 || day==-1)
                return false;
