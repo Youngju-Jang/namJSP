1261b52f9f89f98c5bdc6af293d2d1b9f5d33677
Youngju Jang <joj1043@nate.com>
2023-03-10 11:33:50 +0900

    ch2.20.@GetMapping, @PostMapping -  pom.xml 업데이트

--- src/main/java/com/fastcampus/ch2/RegisterController.java
+++ src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,18 +1,35 @@
 package com.fastcampus.ch2;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 @Controller
 public class RegisterController {
 
-     @RequestMapping("/register/add")
-     public String register(){
-          return "registerForm";
-     }
+//     @RequestMapping ("/register/add")
+//     @GetMapping ("/register/add")
+//     public String register(){
+//          return "registerForm";
+//     }
 	
-     @RequestMapping("/register/save")
-     public String save(){
+//     @RequestMapping(value="/register/save", method = RequestMethod.POST)
+     @PostMapping("/register/save")
+     public String save(User user, Model m) throws Exception {
+          // 1. 유효성 검사
+          if(!isValid(user)){
+               String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
+              return "redirect:/register/add?msg="+msg; // URL 재작성
+          }
+          // 2. DB에 신규회원 정보 저장
           return "registerInfo";
+     }
+     
+     private boolean isValid(User user) {
+          return false;
      }
 }
